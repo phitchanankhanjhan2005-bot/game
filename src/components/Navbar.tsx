35cce38f8b6633f40ad511f3adba2829cabfd1e3
Phitchanan <phitchanankhanjhan2005@gmail.com>
2026-08-19 16:23:14 +0700

    nextjs update

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,26 @@
+//navbar  home/courses/about
+import Link from "next/link";
+
+export default function Navbar() {
+    return (
+        <nav className="navbar">
+            <ul className="navList">
+                <li className="navItem">
+                    <Link href="/" className="navLink">
+                        หน้าแรก
+                    </Link>
+                </li>
+                <li className="navItem">
+                    <Link href="/courses" className="navLink">
+                        รายวิชา
+                    </Link>
+                </li>
+                <li className="navItem">
+                    <Link href="/about" className="navLink">
+                        เกี่ยวกับเรา
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    )
+}
